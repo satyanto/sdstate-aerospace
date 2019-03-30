528f7c1065750a6a1349110a260a68e53651e946
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-03-30 17:33:41 -0500

    play wav test

--- HighJack/AFSK_CLI.py
+++ HighJack/AFSK_CLI.py
@@ -1,6 +1,17 @@
 import os
+import time
+import pygame
+
+pygame.init()
 
 message = "'HELL YEAH BABY THIS CLI PROGRAM WORKS'"
 command = "aprs -c KE0TSL -o clitest.wav " + message
 
 os.system(command)
+
+if os.path.isfile('/home/pi/sdstate-aerospace/HighJack/clitest.wav)'):
+    #file exists
+    aprs_wav = pygame.mixer.Sound('/home/pi/sdstate-aerospace/HighJack/clitest.wav')
+    aprs_wav.play()
+else:
+    #file doesn't exist
