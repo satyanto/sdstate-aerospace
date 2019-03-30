bfe546686f50d33a3ccd60448d5b9795d1e8008f
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-03-30 18:44:41 -0500

    test fix

--- HighJack/AFSK_CLI.py
+++ HighJack/AFSK_CLI.py
@@ -1,12 +1,9 @@
 import os
 import time
-from pydub import AudioSegment
-from pydub.playback import play
 
-message = "'TEST THREE WOOOOOHOOOO'"
-command = "aprs -c KE0TSL -o testthree.wav " + message
+message = "'COMMAND LINE TEST WOOOOOHOOOO'"
+command = "aprs -c KE0TSL -o cmdline.wav " + message
 
 os.system(command)
 
-song = AudioSegment.from_wav("testthree.wav")
-play(song)
+os.system("aplay cmdline.wav")
