f2951d39367bfb23495f63f1eceb35e0e01b72d5
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-03-30 18:53:30 -0500

    test loop

--- HighJack/AFSK_CLI.py
+++ HighJack/AFSK_CLI.py
@@ -7,3 +7,12 @@ command = "aprs -c KE0TSL -o cmdline.wav " + message
 os.system(command)
 
 os.system("aplay cmdline.wav")
+
+i = 1
+while i < 8:
+    msg = "test: "
+    loopmsg = '"{}"'.format(msg)
+    loopcommand = "aprs -c KE0TSL -o cmdline.wav " + loopmsg
+    os.system(loopcommand)
+    os.system("aplay cmdline.wav")
+    time.sleep(4)
