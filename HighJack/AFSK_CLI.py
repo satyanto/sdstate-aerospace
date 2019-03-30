bd02e0a689d7a117cfd52c27f7eae778208c3627
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-03-30 17:28:43 -0500

    added apostrophe

--- HighJack/AFSK_CLI.py
+++ HighJack/AFSK_CLI.py
@@ -1,6 +1,6 @@
 import os
 
-message = "HELL YEAH BABY THIS CLI PROGRAM WORKS"
+message = "'HELL YEAH BABY THIS CLI PROGRAM WORKS'"
 command = "aprs -c KE0TSL -o clitest.wav " + message
 
 os.system(command)
