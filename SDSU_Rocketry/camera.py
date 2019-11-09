c0906353eea9e7c7b1a74bc50e70270d4c077aba
Travis Deegan <spacetrav@5cg8473yx7.jacks.local>
2019-11-09 16:58:47 -0600

    file sorting

--- SDSU_Rocketry/camera.py
+++ SDSU_Rocketry/camera.py
@@ -13,9 +13,9 @@ writevideotickerfile.close()
 
 with picamera.PiCamera() as camera: #setting up camera
         filename = "video"+str(currentvideoticker)
-        camera.start_recording("/home/pi/Development/sdstate-aerospace/SDSU_Rocketry/"+filename+".h264") #start recording
+        camera.start_recording("/home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos/"+filename+".h264") #start recording
         sleep(20) # time of video in seconds
         camera.stop_recording() #recording stopped
-convert = "MP4Box -add /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/+filename+".h264" /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/"+filename+".mp4"
+convert = "MP4Box -add /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos/"+filename+".h264 /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos/"+filename+".mp4"
 call([convert], shell=True) #executing convert using call
 print("Video is Converted")
