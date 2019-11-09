a3bf5077039f6c9cde9ebe18ddcebab7a2a8cfd6
Travis Deegan <spacetrav@5cg8473yx7.jacks.local>
2019-11-09 17:55:41 -0600

    master revision

--- SDSU_Rocketry/master.py
+++ SDSU_Rocketry/master.py
@@ -3,7 +3,7 @@ import csv
 import time
 
 import mpl3115a2
-
+MPL3115A2 = False
 datarows = [
     'Time',
     'Pressure (kPa)',
@@ -14,25 +14,25 @@ datarows = [
 
 filename = 'Data: '+time.strftime('%mm%dd%yy_%%Hh%Mm%Ss')+'.csv'
 
-with open(filename, 'w') as csvfile
-    csvfile = csv.writer(csvfile, deLimiter=',', lineterminator='\n')
+with open(filename, 'w') as csvfile:
+    csvfile = csv.writer(csvfile, delimiter=',', lineterminator='\n')
     
     #write first row
     csvfile.writerow(datarows)
-
+:
 while True:
-
     
     time.sleep(1)
 
     if (MPL3115A2 == True):
         MPL3115A2_Data - mpl3115a2.Get_Data()
-    else
+    else:
         MPL3115A2_Data = [0, 0, 0]
 
-with open(filename, 'a') as datafile
+with open(filename, 'a') as datafile:
     datalogger = csv.writer(datafile, delimiter=',', lineterminator='\n')
     datalogger.writerow([time.strftime('%m/%d/%Y %H:%M:%S%z'),
         str(MPL3115A2_Data[0]),                 # MPL3115A2 Pressure (kPa)
         str(MPL3115A2_Data[1]),                 # MPL3115A2 Temperature (kPa)
         str(MPL3115A2_Data[2]),                 # MPL3115A2 Altitude Estimation (m)
+    ])
