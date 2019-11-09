0fd52b4ca830fd187f004864c6457b6e1a7ffcae
Travis Deegan <spacetrav@5cg8473yx7.jacks.local>
2019-11-09 16:37:26 -0600

    ticker

--- SDSU_Rocketry/camera.py
+++ SDSU_Rocketry/camera.py
@@ -2,10 +2,17 @@ import picamera
 from time import sleep
 from subprocess import call
 
-with picamera.PiCamera() as camera: #setting up camera
-        camera.start_recording("/home/pi/Rocket_Software/video.h264") #start recording
-        sleep(20) # time of video in seconds
-        camera.stop_recording() #recording stopped
-convert = "MP4Box -add /home/pi/Rocket_Software/video.h264 /home/pi/Rocket_Software/new_video.mp4"
-call([convert], shell=True) #executing convert using call
-print("Video is Converted")
+currentvideoticker = 0
+videotickerfile = open("videoticker.txt","r+")
+currentvideoticker = videotickerfile.read()
+videotickerfile.write(currentvideoticker+1)
+videotickerfile.close()
+
+
+#with picamera.PiCamera() as camera: #setting up camera
+#        camera.start_recording("/home/pi/Rocket_Software/video.h264") #start recording
+#        sleep(20) # time of video in seconds
+#        camera.stop_recording() #recording stopped
+#convert = "MP4Box -add /home/pi/Rocket_Software/video.h264 /home/pi/Rocket_Software/new_video.mp4"
+#call([convert], shell=True) #executing convert using call
+#print("Video is Converted")
