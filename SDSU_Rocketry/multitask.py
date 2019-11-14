74a4f50167047d70041640a12266ca4ec135269c
Travis Deegan <spacetrav@same211-00c4lf.jacks.local>
2019-11-14 13:06:58 -0600

    integrating camera in multitask

--- SDSU_Rocketry/multitask.py
+++ SDSU_Rocketry/multitask.py
@@ -4,6 +4,9 @@ import os
 import time
 import subprocess
 import os
+import picamera
+from time import sleep
+from subprocess import call
 
 t=30     #time in seconds
 
@@ -13,11 +16,26 @@ if x:
     timeout = time.time() + t   # t sec from now
 
     while True:
+        currentvideoticker = 0
+        readvideotickerfile = open("videoticker.txt","r")
+        currentvideoticker = int(readvideotickerfile.read())
+        readvideotickerfile.close()
 
-        print('The time is: ', time.time())
+        writevideotickerfile = open("videoticker.txt","w")
+        writevideotickerfile.write(str(currentvideoticker+1))
+        writevideotickerfile.close()
+
+        with picamera.PiCamera() as camera: #setting up camera
+                filename = "video"+str(currentvideoticker)
+                camera.start_recording("/home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos.h264/"+filename+".h264") #start recording
+                time.sleep(t)
+                camera.stop_recording() #recording stopped
+        convert = "MP4Box -add /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos.h264/"+filename+".h264 /home/pi/Development/sdstate-aerospace/SDSU_Rocketry/videos.mp4/"+filename+".mp4"
+        call([convert], shell=True) #executing convert using call
+        print("Video is Converted") 
+        break
         
-        if time.time() > timeout:
-            break
+        
 
 
 else:
